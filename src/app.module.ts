e83c039ef97a313f9c59b6a6db3ebeecb4babcce
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-02-17 09:10:14 -0300

    feat: added more code and comments, learning even more

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 // the @Module() decorator is a function that accepts a single argument which is a metadata object
 // the metadata object is used to provide additional information about the module
 @Module({
   // the imports array is used to import other modules
-  imports: [],
+  imports: [CoffeesModule],
   // the controllers array is used to define the controllers of the module
   controllers: [AppController],
   // the providers array is used to define the providers of the module
---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+// comand to generate a controller: nest g co <name of controller>
+@Controller('coffees')
+export class CoffeesController {
+  // the constructor is a special method that is executed when an instance of the class is created
+  // readonly is used to make the property read-only
+  // the readonly keyword is used to prevent the property from being reassigned
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  // this @Get() decorator will be used to handle GET requests to the /coffees endpoint
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+  // this @Get() decorator will be used to handle GET requests to the /coffees/:id endpoint
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
+  }
+  // this @Post() decorator will be used to handle POST requests to the /coffees endpoint
+  @Post()
+  // the @HttpCode() decorator is used to set the HTTP status code of the response
+  // @HttpCode(HttpStatus.GONE)
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
+  // this @Patch() decorator will be used to handle PATCH requests to the /coffees/:id endpoint
+  @Patch(':id')
+  // the @Param() decorator is used to access the route parameters
+  // the @Body() decorator is used to access the request body
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+  // this @Delete() decorator will be used to handle DELETE requests to the /coffees/:id endpoint
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
